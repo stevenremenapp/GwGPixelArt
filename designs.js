7a865c5d971a05e0969ea2a7985fba718a5c8098
Steven Remenapp <remenapp@gmail.com>
2018-03-30 13:24:01 -0400

    made progress with the js

--- designs.js
+++ designs.js
@@ -1,10 +1,38 @@
 // Select color input
+let colorInput = document.querySelector('#colorPicker').value;
+
 // Select size input
+
+let gridHeight = document.querySelector('#inputHeight').value;
+let gridWidth = document.querySelector('#inputWidth').value;
+
+// Select table
+
+let pixelGrid = document.querySelector('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+  //gridHeight
+
+
+  //gridWidth
+
+
+  //for each number up to gridHeight
+
+  for (let row = 0; row < gridHeight; row++) {
+    console.log('add row to table');
+    for (let column = 0; column < gridWidth; column++) {
+      console.log('add column to table');
+    };
+  };
+
+    //add row to table element
+
+    //for each number up to gridWidth
+
+      //add column to table element
 
 }
