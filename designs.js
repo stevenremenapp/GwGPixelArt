e465f8c903ec2bd7927139b1ddea1636e0a22df0
Steven Remenapp <remenapp@gmail.com>
2018-04-01 21:27:34 -0400

    makeGrid function functioning

--- designs.js
+++ designs.js
@@ -2,60 +2,59 @@
 let colorInput = document.querySelector('#colorPicker');
 
 // Select the selected color
-
 let selectedColor = colorInput.value;
 
-
-
 // Select table
-
 let pixelGrid = document.querySelector('#pixelCanvas');
 
 // Select the Submit button
-
 let submitGrid = document.querySelector('#submitGrid');
 
 //Select the Grid size form
-
 let gridSizeForm = document.querySelector('#sizePicker');
 
-// When size is submitted by the user, call makeGrid()
 
+// When size is submitted by the user, call makeGrid()
 function makeGrid(e) {
 
   // Prevent automatic/normal reload upon submission
   e.preventDefault();
 
-  // Clear current table (by removing tr's?)
-
+  // Clear current table (by removing tbody)
   document.querySelector("tbody").remove();
 
   // Select size inputs
-
   let gridHeight = document.querySelector('#inputHeight').value;
   let gridWidth = document.querySelector('#inputWidth').value;
 
+  //create new tbody element to be used later
+  let tableBody = document.createElement("tbody");
+
   //access gridHeight according to input
   for (let i = 0; i < gridHeight; i++) {
-    //perform action correct number of times -- add click listener to each cell for color
-    console.log('add row to table');
+    //create one tr for each number input
+    //console.log('add row to table');
     let row = document.createElement("tr");
     //access gridWidth according to input
     for (let j = 0; j < gridWidth; j++) {
-      //perform action correct number of times
-      console.log('add column to table');
+      //create one td for each number input
+      //console.log('add column to table');
       let column = document.createElement("td");
+      //append each created td to each created tr
       row.appendChild(column);
+      //append that creation to the created tbody
+      tableBody.appendChild(row);
     };
-    let tableBody = document.createElement("tbody");
-    tableBody.appendChild(row);
+
+    //append created tbody to table
     pixelGrid.appendChild(tableBody);
+
+    // let newTable = pixelGrid.appendChild(tableBody);
+    // tableBody = pixelGrid.replaceChild(newTable, tableBody);
   };
-
-  // pixelGrid = body.replaceChild(pixelGrid, pixelGrid);
-
 };
 
+//upon submission of form run makeGrid() allowing for prevention of default reload action
 gridSizeForm.addEventListener('submit', function(e) {
   makeGrid(e);
 });
