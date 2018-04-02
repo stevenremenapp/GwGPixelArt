6c7f50c504b0b1a40ced8f94c72aa131ff46e719
Steven Remenapp <remenapp@gmail.com>
2018-04-01 20:10:41 -0400

    working but buggy makegrid function

--- designs.js
+++ designs.js
@@ -15,9 +15,20 @@ let pixelGrid = document.querySelector('#pixelCanvas');
 
 let submitGrid = document.querySelector('#submitGrid');
 
+//Select the Grid size form
+
+let gridSizeForm = document.querySelector('#sizePicker');
+
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(e) {
+
+  // Prevent automatic/normal reload upon submission
+  e.preventDefault();
+
+  // Clear current table (by removing tr's?)
+
+  document.querySelector("tbody").remove();
 
   // Select size inputs
 
@@ -25,16 +36,26 @@ function makeGrid() {
   let gridWidth = document.querySelector('#inputWidth').value;
 
   //access gridHeight according to input
-  for (let row = 0; row < gridHeight; row++) {
+  for (let i = 0; i < gridHeight; i++) {
     //perform action correct number of times -- add click listener to each cell for color
     console.log('add row to table');
+    let row = document.createElement("tr");
     //access gridWidth according to input
-    for (let column = 0; column < gridWidth; column++) {
+    for (let j = 0; j < gridWidth; j++) {
       //perform action correct number of times
       console.log('add column to table');
+      let column = document.createElement("td");
+      row.appendChild(column);
     };
+    let tableBody = document.createElement("tbody");
+    tableBody.appendChild(row);
+    pixelGrid.appendChild(tableBody);
   };
 
-}
+  // pixelGrid = body.replaceChild(pixelGrid, pixelGrid);
 
-submitGrid.addEventListener('click', makeGrid);
+};
+
+gridSizeForm.addEventListener('submit', function(e) {
+  makeGrid(e);
+});
